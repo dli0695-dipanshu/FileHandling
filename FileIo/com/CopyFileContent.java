9c7f3d8f227e15108275cb0d4ca66d3b3505092b
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-08-06 17:28:22 +0530

    Copy file content problem

--- FileIo/com/CopyFileContent.java
+++ FileIo/com/CopyFileContent.java
@@ -1,5 +1,23 @@
 package FileIo.com;
 
+import java.io.*;
+
 public class CopyFileContent {
-    
+    public static void main(String[] args) throws IOException {
+        File file=new File("source.txt");
+        if(file.exists()) {
+            FileReader fileReader = new FileReader(file);
+            int i;
+            while ((i=fileReader.read())!=-1){
+
+                System.out.print((char)i);
+            }
+            fileReader.close();
+        }else {
+            FileWriter fileWriter=new FileWriter(file);
+            fileWriter.write("Hello world Hello!");
+            fileWriter.write("\n hello hello");
+            fileWriter.close();
+        }
+    }
 }
